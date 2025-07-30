149746b37dc27a1d63ef3ad370908145eff0f9cb
Michael Scheurer <michael.scheurer@seccom.ch>
2025-07-30 17:36:41 +0200

    add games from api

--- pages/games/index.vue
+++ pages/games/index.vue
@@ -1,64 +1,55 @@
 <script setup lang="ts">
-  import photoMemoryImage from '@/assets/img/games/external/benBrown/photoMemory.png'
+  import { readItems } from '@directus/sdk'
 
-  interface GameDescription {
-    title: string
-    subtitle: string
-    description: string
-    img: string
-    externalUrl: string
-    credits: GameCredit
-  }
+  const api = useApi()
 
-  interface GameCredit {
-    name: string
-    url: string
-  }
-
-  const games: GameDescription[] = [
-    {
-      title: 'Photo Memory O',
-      subtitle: 'Remember the Map!',
-      description: `Du siehst eine Karte mit einem Kontrollpunkt und der Laufrichtung. Erinnere dich auf der nächsten Seite,
-      welches der vier Bilder die Karte zeigt.`,
-      img: photoMemoryImage,
-      externalUrl:
-        'https://orienteering-games.vercel.app/game/photomemory/play',
-      credits: {
-        name: 'Ben Brown',
-        url: 'https://benbrown.dev/',
-      },
-    },
-  ]
+  const { data: games, error } = await useAsyncData('games', () => {
+    return api.directus.request(
+      readItems('Game', {
+        filter: {
+          status: {
+            _eq: 'published',
+          },
+        },
+        fields: ['*', { image: ['*'] }],
+        limit: -1,
+      })
+    )
+  })
 </script>
 
 <template>
   <div class="row">
-    <div v-for="(game, gameIndex) in games" :key="gameIndex" class="col-12">
+    {{ error }}
+    <div
+      v-for="(game, gameIndex) in games"
+      :key="gameIndex"
+      class="col-12 col-md-4"
+    >
       <q-card bordered flat>
-        <q-img :src="game.img" :ratio="1" />
+        <q-img :src="api.getImgUrl(game.image)" :ratio="16 / 9" />
 
         <q-card-section>
-          <div class="text-h6">{{ game.title }}</div>
+          <div class="text-h5">{{ game.title }}</div>
           <div class="text-subtitle2">{{ game.subtitle }}</div>
         </q-card-section>
 
-        <q-card-section>
+        <q-card-section class="text-caption">
           {{ game.description }}
         </q-card-section>
 
         <q-separator dark />
 
         <q-card-actions class="justify-between">
-          <nuxt-link :to="game.credits.url" target="_blank">
-            <q-btn flat size="x-small">Credits {{ game.credits.name }}</q-btn>
+          <nuxt-link :to="game.creditUrl as string" target="_blank">
+            <q-btn flat size="x-small">By {{ game.creditName }}</q-btn>
           </nuxt-link>
           <nuxt-link
             :to="`/games/external?url=${game.externalUrl}`"
             class="col-6"
           >
-            <q-btn class="full-width"
-              ><q-icon name="play_arrow" class="q-pr-xs" /> Spielen</q-btn
+            <q-btn class="full-width">
+              <q-icon name="play_arrow" class="q-pr-xs" /> Spielen</q-btn
             >
           </nuxt-link>
         </q-card-actions>
--- stores/useApi.ts
+++ stores/useApi.ts
@@ -1,6 +1,6 @@
 import { defineStore } from 'pinia'
 import { createDirectus, rest } from '@directus/sdk'
-import type { CustomDirectusTypes } from '../types/DirectusTypes'
+import type { CustomDirectusTypes, DirectusFiles } from '../types/DirectusTypes'
 
 export const useApi = defineStore('useApi', () => {
   const config = useRuntimeConfig()
@@ -8,7 +8,18 @@ export const useApi = defineStore('useApi', () => {
 
   const directus = createDirectus<CustomDirectusTypes>(API_URL).with(rest())
 
+  function getImgUrl(img: DirectusFiles | null | undefined | string): string {
+    if (!img || typeof img === 'string') {
+      throw new Error('Image is not of type Directus File!')
+    }
+    if (!img.id) {
+      throw new Error('image id is not available.')
+    }
+    return `${API_URL}/assets/${img.id}`
+  }
+
   return {
     directus,
+    getImgUrl,
   }
 })
