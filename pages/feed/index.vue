ee638de2db54fcd0dd0d753990fe4ffee8ad0568
Michael Scheurer <michael.scheurer@seccom.ch>
2025-10-27 23:41:06 +0100

    add youtube videos and title only feed entries

--- pages/feed/index.vue
+++ pages/feed/index.vue
@@ -4,92 +4,72 @@
   import solv from '@/assets/img/tmp_feed/solv_logo.jpg'
 
   interface Feed {
+    date: string
     title: string
-    lead: string
-    mainImg: string
-    html: string
+    lead?: string
+    mainImg?: string
+    mainVideo?: string
+    html?: string
   }
 
   const feedEntries: Feed[] = [
     {
+      date: '2025-10-19',
       title: 'Simona Aebersold und Daniel Hubmann in der Nacht am schnellsten',
       lead: 'Heute Samstagabend wurde die Schweizer Meisterschaft im Nacht-Orientierungslauf über den Lichtern der Stadt Zürich, im Zürichberg, ausgetragen. Simona Aebersold und Daniel Hubmann heissen die neue Schweizer Meisterin, beziehungsweise der neue Schweizer Meister.',
       mainImg: mainImg1,
-      html: `Mit dem Zürichberg erwartete die Athletinnen und Athleten auch bei der dritten im Wald stattfindenden Schweizer Meisterschaft typisches Mittelland-Gelände. Zwar ermöglichte das dichte Wegnetz ein hohes Lauftempo, doch galt es, im richtigen Moment das Tempo anzupassen und präzise zum Posten zu navigieren. Abschnitte mit dichten Jungbuchen verschlechterten die Sicht im Dunkeln und stellten eine zusätzliche Herausforderung dar.
-
-Vierter Schweizer-Meister-Titel für Aebersold
-
-Mit dem Sieg bei der Nacht-SM konnte Simona Aebersold alle vier Einzel-Schweizer-Meisterschaften für sich entscheiden und damit den Jackpot des «Swiss Orienteering Grand Slam presented by EGK» knacken. Der Jackpot wird jedes Jahr mit 1000 CHF gefüllt, sofern niemand alle vier Einzel-Schweizer-Meisterschaften für sich entscheidet. Aebersold ist die erste Frau, der dies seit Einführung des Grand Slams gelingt und dadurch 8000 CHF mit nach Hause nehmen darf. «Ich freue mich riesig über den Jackpot! Zwischendurch musste ich etwas zittern: Einen Posten habe ich lange gesucht, da er auf dem Boden lag. Danach musste ich volles Risiko nehmen. Es wurde sehr anstrengend, und mental war es auch nicht einfach, wenn es um so viel Geld geht. Aber ich habe es geschafft», strahlt die frisch gekürte Schweizer Meisterin und Jackpot-Gewinnerin. Zweite in der Kategorie «Damen Elite» wurde Katrin Müller. Das Podest komplettierte Anja Probst.
-
-Hubmann: «Nacht-OL finde ich besonders anstrengen»
-
-Der neue Schweizer Meister im Nacht-OL heisst Daniel Hubmann. Er ist der einzige, dem neben Simona Aebersold das Abräumen des Jackpots bisher gelungen ist, das war 2019. Für ihn fühlte es sich jedoch auch ohne Jackpot-Gewinn sehr gut an, Schweizer Meister zu sein: «Es war ein langer und harter Wettkampf. Ich finde es immer besonders anstrengend, nachts unterwegs zu sein. Trotzdem hatte ich unterwegs bereits die kleine Hoffnung auf den Sieg. Es ist mir mehrmals gelungen, etwas diffuse Posten, die man auch hätte suchen können, genau zu treffen. Das gab ein gutes Gefühl! Jetzt bin ich sehr glücklich, dass es tatsächlich geklappt hat», plauderte der sichtlich zufriedene Hubmann an der Rangverkündigung. Richtig knapp wurde es für Hubmann nicht. Sein Vorsprung auf den Zweitplatzierten, Tino Polsini, betrug knapp eine Minute. Eine weitere Minute später lief Reto Egger auf den dritten Platz.
-
-dsc07258 54863846814 o
-Podest der Elite 
-Matthieu Bührer und Rachel Marxer gewinnen in bei D/H20
-
-Matthieu Bührer gewinnt die dritte Schweizer Meisterschaft in Folge. Auf die Frage, ob er in der Nacht etwas anders gemacht habe, antwortete er: «Ich kontrolliere die Richtung etwas öfter. In der Nacht passiert ein Kompassfehler schneller als am Tag.» 1:14 Minuten hinter Matthieu lief David Baumberger als Zweiter ins Ziel. Dritter wurde Gratian Böhi. Zum ersten Mal diese Saison stand Rachel Marxer zuoberst auf dem Podest einer Schweizer Meisterschaft. Ihr gefiel vor allem die Bahn sehr gut: «Ich fand es toll, dass es auch einige kürzere Posten gab, wo man quer laufen konnte und der Postenraum schwierig war», freute sich Rachel im Ziel. Sie war lediglich 7 Sekunden schneller als die Zweitplatzierte Leonie Mathis. Henriette Radzikowski lief auf den dritten Rang.
-
-Swiss Orienteering Grand Slam presented by EGK
-
-Auch dieses Jahr zählen für die Elite-Athlet*innen sowie die Läufer*innen der Kategorien D/H20 die vier Einzel-Schweizer-Meisterschaften zum Swiss Orienteering Grand Slam presented by EGK. An allen vier Läufen können die Athletinnen und Athleten dank der grosszügigen Unterstützung der EGK Gesundheitskasse und Neutrass attraktive Preisgelder gewinnen. Zudem sammeln sie Punkte, um sich für den Final am 15. November 2025 in Knies Kinderzoo in Rapperswil zu qualifizieren. Die aktuelle Rangliste und somit die Kandidat*innen für den Grand Slam Final kann hier eingesehen werden.
-
-
-Resultate Nacht-SM
-
-Frauen (9,9 km, 290 HM, 24 Po.): 1. Simona Aebersold (Brügg BE) 1:17:50. 2. Katrin Müller (Zürich) 03:07 zurück. 3. Anja Probst (Belp) 03:59. 4. Paula Gross (Niederurnen) 05:26. 5. Sofie Bachmann (Reigoldswil) 05:49. 6. Sanna Hotz (Schafisheim) 06:28.
-
-Männer (12,1 km, 340 HM, 33 Po.): 1. Daniel Hubmann (Bremgarten b. Bern) 1:17:30. 2. Tino Polsini (Gelterkinden) 00:52 Min. zurück. 3. Reto Egger (Rafz) 01:54. 4. Benjamin Wey (Zürich) 03:33. 5. Florian Howald (Bellinzona) 04:22. 6. Pascal Schärer (Mönchaltorf) 04:23.
-
-Alle Resultate
-
-Livelox
-
-Fotos
-
-(Text: Sina Tommer, Fotos: Sandro Anderes, Video: Volltoll)`
     },
     {
+      date: '2025-10-13',
+      title: 'NOM 2025: Wer gewinnt den Titel der letzten Einzel-Schweizer-Meisterschaft der Saison?',
+      lead: 'Mit der Schweizer Meisterschaft im Nacht-OL steht am kommenden Samstag die letzte Einzel-SM der Saison 2025 auf dem Programm. Bei der Elite und den Junior*innen D/H20 geht es auch um den Einzug in den Grand-Slam-Final.',
+      mainVideo: 'fOJHmlCh-wk'
+    },
+    {
+      date: '2025-10-06',
+      title: 'Bewerbung für das Elitekader 2026 verfügbar',
+    },
+    {
+      date: '2025-10-05',
       title: 'Zwei Mal Silber zum Abschluss',
       lead: 'Beim letzten Wettkampf des Jugend Europa Cups (JEC) in Schottland belegten die Schweizer Teams sowohl bei den Juniorinnen als auch bei den Junioren den zweiten Platz. In der Nationenwertung schloss die Schweiz ebenfalls auf dem zweiten Platz ab. Der Sieg ging an Schweden, Finnland war die drittbeste Nation. ',
-      html: `Am letzten Tag des JEC in Aberfoyle, Schottland, zeigte sich die Sonne doch noch. Bei trockenen Verhältnissen wurden die Staffeln sowie die Nationenwertung über alle drei Wettkämpfe entschieden. Der Wald war deutlich besser belaufbar als am Vortag, was zu schnellen Rennen führte.
-
-Schweiz 2 konstant auf Rang 2
-
-Bei den Juniorinnen gelang Rahel Good, die für die Schweiz 2 lief, ein starker Start. Sie kam gemeinsam mit zwei Schwedeninnen im Spitzentrio ins Ziel und übergab an zweiter Stelle, nur eine Sekunde hinter der Leaderin, an Lotta Lüthi. Auch Lotta lief ein gutes Rennen, konnte die Position halten und den Vorsprung auf die Drittplatzierten um eine halbe Minute ausbauen. Henriette Radzikowski lief die Schlussstrecke für Schweiz 2: «Mir sind leider ein paar Fehler passiert. Nichtsdestotrotz bin ich sehr zufrieden mit der Teamleistung und vor allem mit Rahel und Lotta, die mir einen guten Vorsprung gegeben haben.» Es reichte für Rang zwei, knapp fünf Minuten hinter dem Siegerinnenteam Schweden 1.
-
-Da pro Nation nur das jeweils bestplatzierte Team zählt, lief Schweiz 1 mit Leonie Mathis, Rachel Marxer und Seline Sannwald auf den inoffiziellen dritten Rang. Offiziell gewann Norwegen 1 die Bronzemedaille.
-
-Junioren liefen ebenfalls zu Silber
-
-Auch in der Junioren-Staffel gewannen Corsin Müller, Loïc Berger und Matthieu Bührer für Schweiz 1 die Silbermedaille. Corsin gelang ein sauberer Start. Er kam im zweiten Tram zurück, eine knappe Minute hinter der Spitze. Dem zweiten Läufer, Loïc Berger, unterliefen leider ein paar Fehler. Trotzdem konnte er einen Platz gutmachen und Matthieu Bührer als Fünfter, zwei Minuten nach der Spitze, in den Wald schicken. Lediglich die führenden Schweden holte Matthieu nicht mehr auf, was in einer Silbermedaille resultierte. Offiziell gewannen die Dänen die Bronzemedaille, inoffiziell lief Schweden 2 auf den dritten und Schweden 3 auf den vierten Platz.
-
-Kurzfristig kam es bei den Herrenteams noch zu einer Umstellung: Gratian Böhi fiel verletzungsbedingt aus, sodass das vierte Herrenteam mit Flurin Rettich und Lukas Gasser unvollständig war.
-
-Resultate in der Übersicht
-
-Juniorinnen: 2. Schweiz 2 (Rahel Good, Lotta Lüthi, Henriette Radzikowski), 3. Schweiz 1 (Leonie Mathis, Rachel Marxer, Seline Sannwald), 8. Schweiz 4 (Coralie Waldner, Lenia Grimm, Lorenza Rosetti), 13. Schweiz 3 (Mira Werder, Justine Hamel, Sarina Grimm)
-
-Junioren: 2. Schweiz 1 (Corsin Müller, Loïc Berger, Matthieu Bührer), 8. Lavio Müller, Leander Wylenmann, David Baumberger), 22. (Nicolas Mohn, Liam Malnati, Lukas Matthäus), dnf Schweiz 4 (Flurin Rettich, Manuel Gasser)
-
-Die Resultate im Detail sowie das GPS können hier angeschaut werden.
-
-(Text: Sina Tommer)`,
       mainImg: mainImg2
-    }
+    },
   ]
 </script>
 
 <template>
-  <div class="row q-mt-lg">
+  <div class="row q-mt-lg" style="max-width: 600px;">
     <div v-for="(feedEntry, entryId) in feedEntries" :key="entryId" class="col-12">
-      <q-separator v-if="entryId > 0" class="q-mb-md" />
-      <div><img :src="solv" style="height: 30px;"  /></div>
-      <img :src="feedEntry.mainImg" style="max-width: calc(100% + (2*8px)); margin-left: -8px; margin-right: -8px;" />
-      <q-icon name="favorite_outline" size="sm" /> <q-icon name="chat_bubble_outline" size="sm" class="q-ml-sm" /> <q-icon name="mdi-share-outline" size="sm" class="q-ml-sm" />
-      <p class="text-subtitle1 q-mt-sm q-mb-none">{{ feedEntry.title }}</p>
-      <p class="text-caption q-mt-xs" >{{ feedEntry.lead }}</p>
+      <div class="row">
+        <div class="col-12">
+          <q-separator v-if="entryId > 0" class="q-my-lg" />
+        </div>
+        <div class="col-12">
+          <div class="row items-center justify-between">
+            <div class="col-auto">
+              <img :src="solv" style="height: 30px;"  />
+            </div>
+            <div class="col-auto text-grey">
+              {{ new Date(feedEntry.date).toLocaleDateString('de', {dateStyle: 'long'}) }}
+            </div>
+          </div>
+        </div>
+        <div v-if="feedEntry.mainImg" class="col-12"><img :src="feedEntry.mainImg" style="max-width: calc(100% + (2*8px)); margin-left: -8px; margin-right: -8px;" /></div>
+        <div v-if="feedEntry.mainVideo" class="col-12">
+          <iframe
+            :src="`https://www.youtube-nocookie.com/embed/${feedEntry.mainVideo}?controls=0&fs=0&modestbranding=1&playsinline=1&color=white&iv_load_policy=3`"
+            loading="lazy"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+            style="width: calc(100% + (2 * 8px)); margin-left: -8px; margin-right: -8px; height: calc(100vw * 9 / 16);"
+          />
+        </div>
+        <div class="col-12 q-py-sm"><q-icon name="favorite_outline" size="sm" /> <q-icon name="chat_bubble_outline" size="sm" class="q-ml-sm" /> <q-icon name="mdi-share-outline" size="sm" class="q-ml-sm" /></div>
+        <div class="col-12"><p class="text-subtitle1 q-mb-none">{{ feedEntry.title }}</p></div>
+        <div v-if="feedEntry.lead" class="col-12"><p class="text-caption q-mt-xs q-mb-none" >{{ feedEntry.lead }}</p></div>
+      </div>
     </div>
   </div>
 </template>
